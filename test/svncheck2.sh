7ba24a85844341d72efa55fae8386ccb19a56ce3
Eric S. Raymond <esr@thyrsus.com>
2021-03-01 23:08:04 -0500

    Repair and restore a busted test.
    I didn't get svncheck2.sh quite right when I concurremcy-saved it,
    and the bug was nmsked by a silly finger error in the Makefile.

--- test/svncheck2.sh
+++ test/svncheck2.sh
@@ -19,23 +19,19 @@ done
 shift $(($OPTIND - 1))
 {
     trap 'svnwrap' EXIT HUP INT QUIT TERM
-    svinit
-    dir=$(pwd)
-    svnadmin create "$dir/test-repo$$"
-    svn co "file://$dir/test-repo$$" test-checkout
-    tapcd test-checkout
-    mkdir trunk
-    svn add trunk
-    svn commit -m "Create trunk."
-    cd trunk  || (echo "$0: cd failed" >&2; exit 1)
-    mkdir dir1
+    svninit
+    tapcd trunk
+    svn mkdir dir1
     echo "file" > dir1/file
-    svn add dir1
+    svn add dir1/file
     svn commit -m "Create dir1/file."
     svn propset svn:executable '*' dir1/file
     svn commit -m "Make dir1/file executable."
+    svn up
+    ls -l dir1/file
     svn cp dir1 dir2
     svn commit -m "Copy dir1 to dir2."
+    svn up
     ls -l dir1/file dir2/file
     tapcd ../..
 } >/dev/$verbose 2>&1
@@ -43,7 +39,7 @@ shift $(($OPTIND - 1))
 if [ "$dump" = yes ]
 then
     svnadmin dump -q test-repo$$
-elif ls -l test-checkout/trunk/dir2/file | grep x >/dev/null
+elif ls -l test-checkout$$/trunk/dir2/file | grep x >/dev/null
 then
     echo "ok - $0: executable permission is as expected"
 else
