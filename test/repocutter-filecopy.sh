4ca9ca16beeef66bd40152a60dd15271ac66d48b
Eric S. Raymond <esr@thyrsus.com>
2022-01-17 20:10:14 -0500

    Fix a bug in filecopy test option processing.

--- test/repocutter-filecopy.sh
+++ test/repocutter-filecopy.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 ## Test repocutter filecopy resolution
 
-stream=n
-cleanup=y
-while getopts nr opt
+stream=False
+cleanup=True
+while getopts d:nr opt
 do
     case $opt in
+    d) DEBUGOPT="-d $OPTARG";;
     n) stream=False ; cleanup=False ;;
     r) stream=True  ; cleanup=False ;;
     *) echo "$0: unknown option $opt"; exit 1;;
     esac
 done
 
-if [ "$cleanup" = "y" ]
+if [ "$cleanup" = True ]
 then
     trap 'rm -f /tmp/resolved$$' EXIT HUP INT QUIT TERM
 fi
 
-${REPOCUTTER:-repocutter} -q -r 5:6 filecopy <filecopy.svn >/tmp/resolved$$.svn
+# shellcheck disable=SC2086
+${REPOCUTTER:-repocutter} ${DEBUGOPT} -q -r 5:6 filecopy <filecopy.svn >/tmp/resolved$$.svn
 
-if [ "$stream" = "y" ]
+if [ "$stream" = True ]
 then
     cat /tmp/resolved$$.svn
 else
