67b6c497aff45b9a48e11eab6a47550061189dda
Eric S. Raymond <esr@thyrsus.com>
2023-05-28 17:08:12 -0400

    Merge bubble creation in the new test script.

--- test/bubblegen.sh
+++ test/bubblegen.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
-# Create a small test repository with a merge bubbke
-# FIXME: We don't have the right sequence of operationv in bubblegen.sh
+# Create a small test repository with a merge bubble
 
 # shellcheck disable=SC1091
 . ./common-setup.sh
@@ -13,7 +12,7 @@ trap 'rm -fr /tmp/bubble$$' EXIT HUP INT QUIT TERM
 
 # Generic machinery
 
-pseudogit() {
+gitgen() {
     cmd="$1"
     shift
     case "${cmd}" in
@@ -37,7 +36,7 @@ pseudogit() {
 	    fi
 	    ts=$((ts + 60))
 	    ft=$(printf "%09d" ${ts})
-	    # Git seems to reject timestamps with a leading xero
+	    # Git seems to reject timestamps with a leading zero
 	    export GIT_COMMITTER_DATE="1${ft} +0000" 
 	    export GIT_AUTHOR_DATE="1${ft} +0000" 
 	    git commit -q -a -m "$text" --author "Fred J. Foonly <fered@foonly.org>"
@@ -45,11 +44,20 @@ pseudogit() {
 	checkout)
 	    # Checkput branch, crearing if necessary
 	    branch="$1"
+	    # shellcheck disable=SC2086
 	    if [ "$(git branch | grep ${branch})" = "" ]
 	    then
 		git branch "${branch}"
 	    fi
 	    git checkout -q "$1";;
+	merge)
+	    # Perform merge with controlled clock tick.
+	    ts=$((ts + 60))
+	    ft=$(printf "%09d" ${ts})
+	    export GIT_COMMITTER_DATE="1${ft} +0000" 
+	    export GIT_AUTHOR_DATE="1${ft} +0000"
+	    git merge -q "$@"
+	    ;;
 	export)
 	    # Dump export stream
 	    git fast-export --all;;
@@ -58,38 +66,65 @@ pseudogit() {
 
 # The test
 
-pseudogit init /tmp/bubble$$
+# Based on the merge example at
+# https://git-scm.com/book/en/v2/Git-Branching-Basic-Branching-and-Merging
 
-pseudogit commit sample "First commit (master)" <<EOF
+gitgen init /tmp/bubble$$
+
+#C1
+gitgen commit sample "First commit (master)" <<EOF
 First line of sample content.
 EOF
 
-pseudogit commit sample "Second commit (master)" <<EOF
+#C1
+gitgen commit sample "Second commit (master)" <<EOF
 First line of sample content.
 Second line of sample content.
 EOF
 
-pseudogit checkout foobar
-
-pseudogit commit sample2 "Third commit (foobar)" <<EOF
-First line of sample2 content.
-EOF
-
-pseudogit checkout master
-
-pseudogit commit sample "Fourth commit (master)" <<EOF
+#C2
+gitgen commit sample "Third commit (master)" <<EOF
 First line of sample content.
 Second line of sample content.
 Third line of sample content.
 EOF
 
-pseudogit merge foobar
+gitgen checkout iss53
 
-pseudogit commit sample2 "Fifth commit" <<EOF
+#C3
+gitgen commit sample2 "Create new footer [issue 53]" <<EOF
+First line of sample2 content.
+EOF
+
+gitgen checkout master
+
+gitgen checkout hotfix
+
+#C4
+gitgen commit sample3 "Fix broken email address" <<EOF
+First line of sample3 content.
+EOF
+
+gitgen checkout master
+
+gitgen merge hotfix
+
+git branch -q -d hotfix
+
+gitgen checkout iss53
+
+#C5
+gitgen commit sample2 "Create new footer [issue 53]" <<EOF
 First line of sample2 content.
 Second line of sample2 content.
 EOF
 
-pseudogit export
+gitgen checkout master
+
+gitgen merge iss53 -m "Second merge."
+
+#gitk --all
+
+gitgen export
 
 #end
